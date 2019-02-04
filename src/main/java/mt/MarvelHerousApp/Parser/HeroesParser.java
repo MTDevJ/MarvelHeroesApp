be1e455ca3d0fdd8054953bf2ad89ff3375a18a8
MTDevJ <maximtarabychin@yandex.ru>
2019-02-04 17:51:48 +0300

    Added Spring to project

--- src/main/java/mt/MarvelHerousApp/Parser/HeroesParser.java
+++ src/main/java/mt/MarvelHerousApp/Parser/HeroesParser.java
@@ -2,22 +2,23 @@ package mt.MarvelHerousApp.Parser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
 import org.jsoup.select.Elements;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 import java.io.IOException;
-import java.util.HashMap;;
+import java.util.HashMap;
 import java.util.Map;
 
-public class MainHeroesParser {
+public class HeroesParser {
+    private String urlLink;
 
-    public static void main(String[] args) {
-        String url =
-                "https://marvel.fandom.com/ru/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%A1%D1%83%D0%BF%D0%B5%D1%80%D0%B3%D0%B5%D1%80%D0%BE%D0%B8";
-        Map<String, String> heroesLinks = new HashMap<>();
-        heroesLinks = parseHeroes(url, heroesLinks);
-       // heroesLinks.forEach((s, s2) -> System.out.println(s));
-      //  System.out.println(heroesLinks.size() + " Done!");
+    public String getUrlLink() {
+        return urlLink;
+    }
+
+    public void setUrlLink(String urlLink) {
+        this.urlLink = urlLink;
     }
 
     private static Document getDocument(String url) throws IOException {
@@ -25,7 +26,7 @@ public class MainHeroesParser {
                 .get();
     }
 
-    public static Map<String, String> parseHeroes( String url, Map<String, String> heroesLinks){
+    private   Map<String, String> parseHeroes( String url, Map<String, String> heroesLinks){
         try {
             Document document = getDocument(url);
             Elements heroesLinksElements = document.getElementsByClass("category-page__members")
@@ -43,14 +44,26 @@ public class MainHeroesParser {
                     .attr("href");
 
 
-            parseHeroes(nextPage,heroesLinks);
-
+            if (!nextPage.isEmpty() || !nextPage.equals("")) parseHeroes(nextPage,heroesLinks);
             return heroesLinks;
 
         } catch (IOException  e) {
             e.printStackTrace();
-        } catch (NullPointerException  e) {
+        }catch (NullPointerException  e) {
+            e.printStackTrace();
         }
+
         return heroesLinks;
+    }
+
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("app-context.xml");
+        ctx.refresh();
+        HeroesParser parser = ctx.getBean("parserBean", HeroesParser.class);
+
+        Map<String, String> heroesLinks = new HashMap<>();
+        heroesLinks = parser.parseHeroes(parser.urlLink, heroesLinks);
+        System.out.println(heroesLinks.size());
     }
 }
