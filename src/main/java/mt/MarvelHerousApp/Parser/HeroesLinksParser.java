a0d2d0d85cb4aacdbf8aa44fccaa132ec2178bfd
MTDevJ <maximtarabychin@yandex.ru>
2019-02-05 15:44:19 +0300

    -change functionality for HeroesLinksParser class

--- src/main/java/mt/MarvelHerousApp/Parser/HeroesLinksParser.java
+++ src/main/java/mt/MarvelHerousApp/Parser/HeroesLinksParser.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 public class HeroesLinksParser {
     private String urlLink;
-
     public String getUrlLink() {
         return urlLink;
     }
@@ -27,8 +26,8 @@ public class HeroesLinksParser {
             Document document = Connector.getDocument(url);
             Elements heroesLinksElements = document.getElementsByClass("category-page__members")
                     .select("a");
-
-            heroesLinksElements.forEach(
+            heroesLinksElements
+                    .forEach(
                     element ->
                         heroesLinks.add( element.absUrl("href"))
 
@@ -59,11 +58,12 @@ public class HeroesLinksParser {
             Document document = Connector.getDocument(heroLink);
             Element element = document.getElementsByTag("aside").first();
 
-            hero.setName(getName(element));
-            hero.setImageLink(getImageLink(element));
-            hero.setNickname(getNickName(element));
-            hero.setKind(getKind(element));
-
+            if (element != null) {
+                hero.setName(getName(element));
+                hero.setImageLink(getImageLink(element));
+                hero.setNickname(getNickName(element));
+                hero.setKind(getKind(element));
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -71,10 +71,15 @@ public class HeroesLinksParser {
     }
 
     private static String getKind(Element element) {
-        String kind = element.getElementsByAttributeValue("data-source","Вид")
-                .first()
-                .getElementsByClass("pi-data-value pi-font")
-                .text();
+        String kind;
+        try {
+            kind = element.getElementsByAttributeValue("data-source","Вид")
+                    .first()
+                    .getElementsByClass("pi-data-value pi-font")
+                    .text();
+        } catch (NullPointerException e) {
+            return "-Информация отсутствует-";
+        }
         return !kind.isEmpty()? kind : "-Информация отсутствует-";
     }
 
@@ -84,19 +89,37 @@ public class HeroesLinksParser {
     }
 
     private static String getImageLink(Element element) {
-        Element imageElement = element.getElementsByAttributeValue("data-source","Картинка")
-                .first();
 
-         String imageURL = imageElement
-                .child(0)
-                .absUrl("href");
+        String imageURL;
+        try {
+            Element imageElement = element.getElementsByAttributeValue("data-source","Картинка")
+                    .first();
 
+            imageURL = imageElement
+                   .child(0)
+                   .absUrl("href");
+        } catch (NullPointerException e) {
+            return "-Изображение отсутствует-";
+        }
         return !imageURL.isEmpty() ? imageURL : "-Изображение отсутствует-";
     }
 
     private static String getName(Element element) {
        String name = element.getElementsByAttributeValue("data-source","Имя").text();
         return !name.isEmpty()? name : "-Информация отсутствует-";
+    }
+
+    private static List<Hero> getHeroes(List<String> heroesLinks) {
+        List<Hero> heroes = new ArrayList<>();
+        heroesLinks.forEach(link -> {
+            Hero hero = getHero(link);
+           if (hero.getName() != null) {
+               heroes.add(hero);
+               System.out.println("Персонаж " + hero.getName() + "добавлен в список");
+           }
+        });
+
+        return heroes;
     }
 
     public static void main(String[] args) {
@@ -108,9 +131,9 @@ public class HeroesLinksParser {
         List<String> heroesLinks = new ArrayList<>();
         heroesLinks = parser.parseLinks(parser.urlLink, heroesLinks);
 
-        /*for (int i = 0; i < 5; i++) {
-            Hero hero = getHero(heroesLinks.get(i));
-            System.out.println(hero.toString());
-        }*/
+        List<Hero> heroes = getHeroes(heroesLinks);
+
+        System.out.println("Итого персонажей " + heroes.size());
+
     }
 }
